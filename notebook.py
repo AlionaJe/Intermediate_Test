4c8af4e5fa37da14e7642aa94c99bb97c09ace57
Aliona <aljereghi@gmail.com>
2024-01-22 19:11:06 +0200

    Added class NoteApp

--- notebook.py
+++ notebook.py
@@ -8,3 +8,19 @@ class Note:
         self.title = title
         self.body = body
         self.timestamp = timestamp
+
+class NoteApp:
+    def __init__(self, file_path='notes.json'):
+        self.file_path = file_path
+        self.notes = self.load_notes()
+
+    def load_notes(self):
+        if os.path.exists(self.file_path):
+            with open(self.file_path, 'r') as file:
+                notes_data = file.read().splitlines()
+                return [self.parse_json(note_data) for note_data in notes_data]
+        return []
+
+    def parse_json(self, json_string):
+        note_data = json.loads(json_string)
+        return Note(**note_data)
