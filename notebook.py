b877a48a204ef90a4eff94e844b0532767c54f84
Aliona <aljereghi@gmail.com>
2024-01-22 18:52:53 +0200

    Added file notebook

---
+++ notebook.py
@@ -0,0 +1,10 @@
+import json
+import os
+import datetime
+
+class Note:
+    def __init__(self, note_id, title, body, timestamp):
+        self.note_id = note_id
+        self.title = title
+        self.body = body
+        self.timestamp = timestamp
